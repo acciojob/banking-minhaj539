dd0b17173236031a15972aea83da534aa03eafdc
minhaj539 <mdminhaj8383@gmail.com>
2022-12-30 18:46:33 +0530

    updated

--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -4,9 +4,17 @@ public class StudentAccount extends BankAccount{
 
     String  institutionName;
 
+    public String getInstitutionName() {
+        return institutionName;
+    }
+
+    public void setInstitutionName(String institutionName) {
+        this.institutionName = institutionName;
+    }
+
     public StudentAccount(String name, double balance, String  institutionName) {
         super(name,balance,0);
-        this.institutionName=institutionName;
+        setInstitutionName(institutionName);
         //minimum balance is 0 by default
 
     }
